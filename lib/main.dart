c543a084d63811b0282aac824d4ed0bf3d57fddd
Yousef Montash <yousefmontash2002@gmail.com>
2023-07-24 02:57:53 +0300

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  
+  runApp(NotesApp());
 }
 
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
