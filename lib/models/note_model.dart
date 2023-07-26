e1a7c86f996776d34b831bbea42cb4113fe3f3d1
Yousef Montash <yousefmontash2002@gmail.com>
2023-07-27 01:00:58 +0300

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,11 @@
+class NoteModel {
+  final String tiltle;
+  final String content;
+  final String date;
+  final int color;
+  NoteModel({ required this.color, 
+     required this.tiltle,
+    required this.content,
+    required this.date,
+  });
+}
