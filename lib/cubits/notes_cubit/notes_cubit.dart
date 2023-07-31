8eb10cd1a14266dbb2ffc40db728e68ada9f4c34
Yousef Montash <yousefmontash2002@gmail.com>
2023-07-31 04:37:35 +0300

    setting initial state

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,9 +1,16 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/models/note_model.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+  List<NoteModel>? notes;
+  fetchAllNotes() {
+    var noteBox = Hive.box<NoteModel>(kNotesBox);
+    notes = noteBox.values.toList();
+  }
 }
--- lib/cubits/notes_cubit/notes_state.dart
+++ lib/cubits/notes_cubit/notes_state.dart
@@ -3,18 +3,6 @@ part of 'notes_cubit.dart';
 @immutable
 abstract class NotesState {}
 
-class NotesInitial extends NotesState {}
-
-class NotesLoading extends NotesState {}
-
-class NotesSucceed extends NotesState {
-  final List<NoteModel> notes;
-
-  NotesSucceed(this.notes);
-}
-
-class NotesFailed extends NotesState {
-  final String errMessage;
-
-  NotesFailed(this.errMessage);
+class NotesInitial extends NotesState {
+  
 }
